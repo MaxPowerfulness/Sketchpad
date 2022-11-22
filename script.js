59dc25150ddfd05f586a8d115856d0f78db990d0
Sean Gombart <skgombart@gmail.com>
2022-11-21 20:46:36 -0800

    Add a slider on the side menu that displays the grid's dimensions

--- script.js
+++ script.js
@@ -4,20 +4,35 @@ const resetBtn = document.querySelector('.reset');
 let cols = 16;
 let rows = 16;
 
+// Slider
+const slider = document.getElementById('sliderRange');
+const output = document.querySelector('.width');
+const output2 = document.querySelector('.height')
+output.innerHTML = slider.value;
+output2.innerHTML = slider.value;
+
+
+
+// Changes the display for the dimensions of the grid.
+function displayDimensions() {
+    output.innerHTML = slider.value;
+    output2.innerHTML = slider.value;
+}
+
 // Creates a number of square divs that get arranged into a grid. 
 function createGrid(cols, rows) {
     for (let i = 0; i < (cols * rows); i++) {
         const div = document.createElement('div');
         div.classList.add('square');
         div.setAttribute('style', 'border: 1px solid black;');
-        div.addEventListener('click', function() {
+        div.addEventListener('mousedown', function() {
             div.classList.toggle('squareFill');
         });
         container.appendChild(div);
     }
 };
 
-// Changes the div color when clicked
+// Clears the grid back to the default state of no color
 function resetGrid() {
     const gridDivs = document.querySelectorAll('.squareFill') 
     gridDivs.forEach((div) => {
