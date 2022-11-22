b757f46662540e2cd24979d74d783ff9fcdbe1c6
Sean Gombart <skgombart@gmail.com>
2022-11-21 23:34:57 -0800

    Make grid scalable based off the dimensions given in the slider

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-// Global elements
-const container = document.querySelector('#container');
-const resetBtn = document.querySelector('.reset');
-let cols = 16;
-let rows = 16;
-
 // Slider
 const slider = document.getElementById('sliderRange');
 const output = document.querySelector('.width');
 const output2 = document.querySelector('.height')
-output.innerHTML = slider.value;
-output2.innerHTML = slider.value;
+output.innerHTML = slider.value; // First number of dimension
+output2.innerHTML = slider.value; // Second number of dimension
 
+// Global elements
+const container = document.getElementById('gridContainer');
+const resetBtn = document.querySelector('.reset');
+let area = slider.value**2;
 
-
-// Changes the display for the dimensions of the grid.
+/* 
+Changes the display that states the dimensions of the grid and changes
+the actual dimensions of the grid when the slider is moved.
+*/
 function displayDimensions() {
     output.innerHTML = slider.value;
     output2.innerHTML = slider.value;
+    resetGrid();
+    updateGrid();
 }
 
 // Creates a number of square divs that get arranged into a grid. 
-function createGrid(cols, rows) {
-    for (let i = 0; i < (cols * rows); i++) {
+function createGrid(area) {
+    let length = area**0.5;
+    container.setAttribute('style', `grid-template-columns: repeat(${length}, 1fr);`);
+    for (let i = 0; i < area; i++) {
         const div = document.createElement('div');
         div.classList.add('square');
         div.setAttribute('style', 'border: 1px solid black;');
@@ -32,8 +36,27 @@ function createGrid(cols, rows) {
     }
 };
 
-// Clears the grid back to the default state of no color
+/*
+Removes all the grid items (div blocks) to allow the new amount of grid items
+to be appended into the grid contianer. This prevents the additional grid items
+to append to the currently present ones. 
+*/
 function resetGrid() {
+    let child = container.lastElementChild;
+    while (child) {
+        container.removeChild(child);
+        child = container.lastElementChild;
+    }
+}
+
+// Updates the grid size of the display
+function updateGrid() {
+    area = slider.value**2;
+    createGrid(area);
+}
+
+// Clears the grid back to the default state of no color
+function clearGrid() {
     const gridDivs = document.querySelectorAll('.squareFill') 
     gridDivs.forEach((div) => {
         div.classList.remove('squareFill');
@@ -41,5 +64,5 @@ function resetGrid() {
 }
 
 
-createGrid(cols, rows);
-resetBtn.addEventListener('click', resetGrid);
+createGrid(area);
+resetBtn.addEventListener('click', clearGrid);
