20c35c20a045939c4a118763b59548b5ccce3ee7
Sean Gombart <skgombart@gmail.com>
2022-11-26 10:07:59 -0800

    Make separate function for filling each square in grid. Make function for changing the color of the fill.

--- script.js
+++ script.js
@@ -5,11 +5,25 @@ const output2 = document.querySelector('.height')
 output.innerHTML = slider.value; // First number of dimension
 output2.innerHTML = slider.value; // Second number of dimension
 
+
 // Global elements
 const container = document.getElementById('gridContainer');
 const resetBtn = document.querySelector('.reset');
+const fillColor = document.querySelector('#fillColor');
 let area = slider.value**2;
 
+// Initialize
+createGrid(area);
+let gridItems = container.querySelectorAll('div.squareTransparent');
+fillColor.addEventListener('input', function() {  // Event listener for color wheel
+    console.log(fillColor.value)
+    changeColor();
+});
+resetBtn.addEventListener('click', clearGrid);  // Reset button resets the grid
+
+
+
+//Functions defined
 /* 
 Changes the display that states the dimensions of the grid and changes
 the actual dimensions of the grid when the slider is moved.
@@ -27,14 +41,25 @@ function createGrid(area) {
     container.setAttribute('style', `grid-template-columns: repeat(${length}, 1fr);`);
     for (let i = 0; i < area; i++) {
         const div = document.createElement('div');
-        div.classList.add('square');
+        div.classList.add('squareTransparent');
         div.setAttribute('style', 'border: 1px solid black;');
-        div.addEventListener('mousedown', function() {
-            div.classList.toggle('squareFill');
-        });
+        div.addEventListener('mousedown', function () {
+            div.style.backgroundColor = 'black'
+        })
         container.appendChild(div);
     }
 };
+
+// Fill color
+function changeColor() {
+    gridItems = container.querySelectorAll('div.squareTransparent');
+    gridItems.forEach((item) => { 
+        item.addEventListener('mousedown', () => {
+            item.setAttribute('style',`background: ${fillColor.value}`);
+        });
+    });
+};
+
 
 /*
 Removes all the grid items (div blocks) to allow the new amount of grid items
@@ -57,12 +82,11 @@ function updateGrid() {
 
 // Clears the grid back to the default state of no color
 function clearGrid() {
-    const gridDivs = document.querySelectorAll('.squareFill') 
+    let gridDivs = container.querySelectorAll('div')
     gridDivs.forEach((div) => {
-        div.classList.remove('squareFill');
+        div.setAttribute('style', 'background: transparent');
     });
 }
 
 
-createGrid(area);
-resetBtn.addEventListener('click', clearGrid);
+
